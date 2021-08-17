7c3f21de9514fec0d35f3a097ba86a7a8f161de9
ShyanM <shyanmuscat24@gmail.com>
2021-08-17 23:49:23 +0000

    Create pages for adding recipe and individual recipe

--- app.py
+++ app.py
@@ -102,6 +102,17 @@ def logout():
     return redirect(url_for("login"))
 
 
+# Individual Recipe page
+@app.route("/ind_recipe/<recipe_id>")
+def recipe(recipe_id):
+
+    return render_template("ind_recipe.html")
+
+
+@app.route("/add_recipe")
+def add_recipe():
+    return render_template("add_recipe.html")
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
